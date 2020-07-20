f503be77d7ad164523b05c4f49564484fc81e28a
adehikmat <aalolxyz@gmail.com>
2020-07-20 19:43:03 +0700

    coba

--- src/pages/movies.js
+++ src/pages/movies.js
@@ -43,17 +43,33 @@ import { getAllfilmCreator } from "../redux/actions/actionFilm";
 //   }
 // }
 
-
-
 class Movies extends Component {
   componentDidMount() {
     this.props.getAllfilmCreator();
   }
   render() {
+    const { loading, data, error } = this.props;
+    const film =
+      data.length > 0 ? data.filter((movies) => movies.category.id === 2) : [];
     return (
       <>
         <Header />
         <Jumbotron />
+        {loading ? (
+          <h1 className="loading">Loading.....</h1>
+        ) : error ? (
+          <h1 className="loading">{error}</h1>
+        ) : data.length > 0 ? (
+          <>
+            <ListFilm
+              kategori="Movies"
+              data={data.length > 0 && !loading ? film : []}
+            />
+          </>
+        ) : (
+          <h1 className="loading"> Film Blank </h1>
+        )}
+        <Footer created="Ade Hikmat Pauji Ridwan" />
       </>
     );
   }
